06b7e7b0954db0b9863640af7fff9e7d35b422c3
Gabriel Domingues <gabrielleao.gd@gmail.com>
2026-09-15 07:51:53 -0300

    First commit

---
+++ src/test/java/com/construcaosostware/ws_viaja_junto/WsViajaJuntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construcaosostware.ws_viaja_junto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsViajaJuntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
